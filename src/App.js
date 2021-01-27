7ce559353dced848887a2c808d5a7618a1562331
osmancalisir <osman.calisir@hotmail.com>
2021-01-27 10:02:31 +0300

    data.json fixed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MaterialTable from 'material-table';
 
 const App = () => {
   
-  const data = [
-        {id:1,first_name:"Jennine",last_name:"Fivey"},
-        {id:2,first_name:"Vivie",last_name:"Pietesch"},
-        {id:3,first_name:"Neila",last_name:"Darwen"},
-        {id:4,first_name:"Salvatore",last_name:"MacIlraith"},
-        {id:5,first_name:"Jaine",last_name:"Ealles"},
-        {id:6,first_name:"Melisandra",last_name:"Franz"},
-        {id:7,first_name:"Kanya",last_name:"Beilby"},
-        {id:8,first_name:"Shanan",last_name:"Robens"},
-        {id:9,first_name:"Carlie",last_name:"Hepher"},
-        {id:10,first_name:"Adolphus",last_name:"Liggett"},
-        {id:11,first_name:"Jania",last_name:"Runacres"},
-        {id:12,first_name:"Carmelia",last_name:"Wyrill"},
-        {id:13,first_name:"Marilee",last_name:"Bleasdale"},
-        {id:14,first_name:"Julietta",last_name:"Dundredge"},
-        {id:15,first_name:"Rusty",last_name:"Bartolozzi"},
-        {id:16,first_name:"Fawnia",last_name:"Wintour"},
-        {id:17,first_name:"Jeremy",last_name:"Minifie"},
-        {id:18,first_name:"Kane",last_name:"idewell"},
-        {id:19,first_name:"Maryjo",last_name:"Rekes"},
-        {id:20,first_name:"Karina",last_name:"Button"}
-]
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+  async function getData() {
+    fetch(
+      "https://raw.githubusercontent.com/apiCoat/coatOfTable/master/src/data/data.json"
+    )
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (txt) {
+        let d = txt.replace(/first_name/g, `"First Name"`);
+        d = d.replace(/last_name/g, `"Last Name"`);
+        d = JSON.parse(d);
+        setData(d);
+      });
+  }
+  getData();
+  }, []); 
 
   const columns = [
     {
